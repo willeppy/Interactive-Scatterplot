91f549fe9b50b787be7b7bffb4e6f46b24dd5a6f
Will Epperson <willepp@live.com>
2020-10-20 16:34:08 -0400

    checkpoint - NOT working but save

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,31 @@
 <link rel="stylesheet" href="custom.css">
 
 
-<style>
-	.block {
-		width: 10rem;
-		height: 10rem;
-		display: inline-block;
-		color: #ffff;
-		padding: 1rem;
-	}
-	.one {
-		background-color: #af3a3a;
-	}
-
-	.two {
-		background-color: #373790;
-	}
-	
-</style>
-
 <script>
 	// imports
 	import Chart from "./Chart.svelte";
+	import Chart2 from "./Chart2.svelte";
 	import * as data_json from "./data_dir/data.json";
-
-	// console.log(data_json); 
 
 	let col_keys = Object.keys(data_json["values"][0]); 
 	let var_col = col_keys[0];
 
-	function handleHover(i){
-		var_col=col_keys[i];
-	}
+	// function handleHover(i){
+	// 	var_col=col_keys[i];
+	// }
+
+	// var b_1_c = 0;
+	// var b_2_c = 0;
+
+	// function handleClick(b_i) {
+	// 	if(b_i == 1) {
+	// 		b_1_c += 1;
+	// 	}
+
+	// 	if(b_i == 2) {
+	// 		b_2_c += 1;
+	// 	}
+	// }
 </script>
 
 <main>
@@ -39,8 +33,16 @@
 	<p>Hover over a scatterplot to bring that variable into focus. 
 		<a href="https://github.com/willeppy/Interactive-Scatterplot"> See the code.</a></p>
 
-	{#each col_keys as static_col, i}
+	<!-- {#each col_keys as static_col, i}
 		<Chart on:mouseenter={() => handleHover(i)} id={i} c_data={data_json} {static_col} {var_col}/>
-	{/each}
+	{/each} -->
+
+	<!-- <button on:click={() => handleClick(1)}> Click me 1 </button> -->
+	<!-- <button on:click={() => handleClick(2)}> Click me 2 </button> -->
+
+	<!-- <Chart2 id={1} count={b_1_c}/> -->
+	<!-- <Chart2 id={2} count={b_2_c}/> -->
+	<Chart2/>
+
 
 </main>
--- src/Chart.svelte
+++ src/Chart.svelte
@@ -1,30 +1,55 @@
 <script>
+    import "../public/gemini.web.js";
 
     export let c_data;
     export let static_col;
     export let var_col;
     export let id;
 
-    var s = {};
-    $: console.log("This was run. static is " + static_col + " var is " + var_col)
+    var s_old_vega = {};
 
-    function updateChart(sc, vc) {
+    // $: console.log("This was run. static is " + static_col + " var is " + var_col)
+
+    const gs = {
+            "timeline": {
+            "concat": [
+                {
+                "sync": [
+                    {
+                    "component": {"mark": "marks"},
+                    "change": {
+                        "scale": ["x", "y"],
+                        "data": true,
+                        "encode": {"update": true, "enter": true, "exit": true},
+                        "marktype": true
+                    },
+                    "timing": {"duration": {"ratio": 1}}
+                    },
+                    {"component": {"axis": "x"}, "timing": {"duration": {"ratio": 1}}},
+                    {"component": {"axis": "y"}, "timing": {"duration": {"ratio": 1}}}
+                ]
+                }
+            ]
+            },
+            "totalDuration": 2000
+        };
+    
+    console.log("Is vega embed being run in " + id)
+    vegaEmbed('#vis_' + id, gs);
+
+
+    function updateChart(sc, vc, gem_sch) {
+        let s_new;
 
         if (sc == vc) {
-        
-        s = {
-            $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
-            data: c_data,
-            mark: "bar",
-            encoding: {
-                x: {"bin": true, "field": sc},
-                y: {"aggregate": "count"}
+            s_new = {
+                $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+                data: c_data,
+                mark: "bar",
+                encoding: { x: {"bin": true, "field": sc}, y: {"aggregate": "count"}}
             }
-        }
-
         } else {
-
-            s = {
+            s_new = {
                 $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
                 data: c_data,
                 mark: {type:'point', filled:true},
@@ -34,11 +59,33 @@
             };
         }
 
-        vegaEmbed('#vis_' + id, s);
+        // var s_old_vega = gemini.vl2vg4gemini(s_old)
+        let s_new_vega = gemini.vl2vg4gemini(s_new);
+
+        // initially empty
+        if(s_old_vega.constructor === Object && Object.keys(s_old_vega).length === 0) {
+            //console.log("Setting the old vega var for first time in Chart "+id)
+            s_old_vega = s_new_vega;
+        }
+
+        async function play(ov, nv, gss) {
+            console.log("In play function for for Chart #vis_" + id)
+            console.log("OV:",ov)
+            console.log("NV:",nv)
+            let anim = await gemini.animate(ov, nv, gss);
+            await anim.play('#vis_' + id);
+            
+            // update 
+            s_old_vega = nv;
+        }
+
+        play(s_old_vega, s_new_vega, gem_sch);
 
     }
 
-    $: updateChart(static_col, var_col)
+    $: updateChart(static_col, var_col, gs)
+
+    // console.log("OV outside of func scope: ", s_old_vega)
     
    
 </script>
