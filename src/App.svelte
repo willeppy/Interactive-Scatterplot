f4d7bb2b0dc11f728a24b791a775442d4b1eea5d
Will Epperson <willepp@live.com>
2020-10-20 17:48:56 -0400

    seems like should work but doesnt -- save checkpoint

--- src/App.svelte
+++ src/App.svelte
@@ -3,23 +3,24 @@
 
 <script>
 	// imports
-	// import Chart from "./Chart.svelte";
-	import Chart4 from "./Chart4.svelte";
+	import Chart from "./Chart.svelte";
+	// import Chart4 from "./Chart4.svelte";
 
 	import * as data_json from "./data_dir/data.json";
 
 	let col_keys = Object.keys(data_json["values"][0]); 
 	let var_col = col_keys[0];
 
-	// function handleHover(i){
-	// 	var_col=col_keys[i];
-	// }
-
-	let i = 0;
-
-	function handleClick() {
-		i++;
+	function handleHover(i){
+		var_col=col_keys[i];
+		console.log("VAR COL IS NOW: ", var_col)
 	}
+
+	// let i = 0;
+
+	// function handleClick() {
+	// 	i++;
+	// }
 </script>
 
 <main>
@@ -27,14 +28,14 @@
 	<p>Hover over a scatterplot to bring that variable into focus. 
 		<a href="https://github.com/willeppy/Interactive-Scatterplot"> See the code.</a></p>
 
-	<!-- {#each col_keys as static_col, i}
+	{#each col_keys as static_col, i}
 		<Chart on:mouseenter={() => handleHover(i)} id={i} c_data={data_json} {static_col} {var_col}/>
-	{/each} -->
+	{/each}
 	
-	<button on:click={handleClick}> Click me to change both </button>
+	<!-- <button on:click={handleClick}> Click me to change both </button>
 
 	<Chart4 c_id={1} {i}/>
-	<Chart4 c_id={2} {i}/>
+	<Chart4 c_id={2} {i}/> -->
 
 
 </main>
--- src/Chart.svelte
+++ src/Chart.svelte
@@ -1,95 +1,108 @@
 <script>
-    import "../public/gemini.web.js";
+    // import "../public/gemini.web.js";
 
     export let c_data;
     export let static_col;
     export let var_col;
     export let id;
 
-    var s_old_vega = {};
-
-    // $: console.log("This was run. static is " + static_col + " var is " + var_col)
-
+    // const
     const gs = {
-            "timeline": {
-            "concat": [
+        timeline: {
+            concat: [
                 {
-                "sync": [
-                    {
-                    "component": {"mark": "marks"},
-                    "change": {
-                        "scale": ["x", "y"],
-                        "data": true,
-                        "encode": {"update": true, "enter": true, "exit": true},
-                        "marktype": true
-                    },
-                    "timing": {"duration": {"ratio": 1}}
-                    },
-                    {"component": {"axis": "x"}, "timing": {"duration": {"ratio": 1}}},
-                    {"component": {"axis": "y"}, "timing": {"duration": {"ratio": 1}}}
-                ]
-                }
-            ]
-            },
-            "totalDuration": 2000
-        };
+                    sync: [
+                        {
+                            component: { mark: "marks" },
+                            change: {
+                                scale: ["x", "y"],
+                                data: true,
+                                encode: {
+                                    update: true,
+                                    enter: true,
+                                    exit: true,
+                                },
+                                marktype: true,
+                            },
+                            timing: { duration: { ratio: 1 } },
+                        },
+                        {
+                            component: { axis: "x" },
+                            timing: { duration: { ratio: 1 } },
+                        },
+                        {
+                            component: { axis: "y" },
+                            timing: { duration: { ratio: 1 } },
+                        },
+                    ],
+                },
+            ],
+        },
+        totalDuration: 2000,
+    };
+
+    // init
+    var s_old_vega = gemini.vl2vg4gemini({
+        $schema: "https://vega.github.io/schema/vega-lite/v4.json",
+        data: c_data,
+        mark: "bar",
+        encoding: {
+            x: { bin: true, field: static_col },
+            y: { aggregate: "count" },
+        },
+    });
+
+
+    // func
+    async function play(ov, nv) {
+        console.log("In play function for for Chart #vis_" + id);
+        console.log("OV:", ov);
+        console.log("NV:", nv);
+        let anim = await gemini.animate(ov, nv, gs);
+        await anim.play("#vis_" + id);
+        
+        //return "done.";
+    }
     
-    console.log("Is vega embed being run in " + id)
-    vegaEmbed('#vis_' + id, gs);
-
-
-    function updateChart(sc, vc, gem_sch) {
+    function updateChart(sc, vc) {
         let s_new;
 
         if (sc == vc) {
             s_new = {
-                $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+                $schema: "https://vega.github.io/schema/vega-lite/v4.json",
                 data: c_data,
                 mark: "bar",
-                encoding: { x: {"bin": true, "field": sc}, y: {"aggregate": "count"}}
-            }
+                encoding: {
+                    x: { bin: true, field: sc },
+                    y: { aggregate: "count" },
+                },
+            };
         } else {
             s_new = {
-                $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+                $schema: "https://vega.github.io/schema/vega-lite/v4.json",
                 data: c_data,
-                mark: {type:'point', filled:true},
+                mark: { type: "point", filled: true },
                 encoding: {
-                    x: { field: sc, type:"quantitative"}, y: {field: vc, type:"quantitative"}
-                }
+                    x: { field: sc, type: "quantitative" },
+                    y: { field: vc, type: "quantitative" },
+                },
             };
         }
 
-        // var s_old_vega = gemini.vl2vg4gemini(s_old)
         let s_new_vega = gemini.vl2vg4gemini(s_new);
 
-        // initially empty
-        if(s_old_vega.constructor === Object && Object.keys(s_old_vega).length === 0) {
-            //console.log("Setting the old vega var for first time in Chart "+id)
-            s_old_vega = s_new_vega;
-        }
-
-        async function play(ov, nv, gss) {
-            console.log("In play function for for Chart #vis_" + id)
-            console.log("OV:",ov)
-            console.log("NV:",nv)
-            let anim = await gemini.animate(ov, nv, gss);
-            await anim.play('#vis_' + id);
-            
-            // update 
-            s_old_vega = nv;
-        }
-
-        play(s_old_vega, s_new_vega, gem_sch);
-
+        play(s_old_vega, s_new_vega);
+        
+        // update
+        s_old_vega = s_new_vega;
     }
 
-    $: updateChart(static_col, var_col, gs)
+    // console.log("Vega embed being run in " + id + " with... ");
+    // console.log(s_old_vega);
+    vegaEmbed("#vis_" + id, s_old_vega);
 
-    // console.log("OV outside of func scope: ", s_old_vega)
-    
-   
+    $: updateChart(static_col, var_col);
 </script>
 
 <!-- The mouse enter with no functon forwards this event to app I think -->
-<div on:mouseenter id="vis_{id}"></div> 
-
+<div on:mouseenter id="vis_{id}" />
