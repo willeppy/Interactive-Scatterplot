454a145ea6386200d5232cc70cfe5d7f2a02b26a
Will Epperson <willepp@live.com>
2020-10-23 14:10:08 -0400

    clean up imports

--- src/Chart.svelte
+++ src/Chart.svelte
@@ -1,4 +1,6 @@
 <script>
+    import embed from "vega-embed";
+
     export let c_data;
     export let static_col;
     export let var_col;
@@ -52,7 +54,7 @@
     var new_c = old_c;
     let viewtag = "#view_" + c_id;
 
-    vegaEmbed(viewtag, old_c, { renderer: "svg" });
+    embed(viewtag, old_c, { renderer: "svg" });
 
     function updateChart(sc, vc) {
         let s_new;
@@ -81,19 +83,24 @@
 
         new_c = gemini.vl2vg4gemini(s_new);
 
-        play(old_c, new_c);
+        play(old_c, new_c).then((res) => {
+            console.log("Finished play in " + res)
+        }).catch((err) => {
+            console.log("ERROR: ", err)
+        });
 
         // update
         old_c = new_c;
     }
 
     async function play(oc, nc) {
-        console.log("In play for "+viewtag);
-        console.log("OLD_C ", oc);
-        console.log("NEW_C ", nc);
+        // console.log("In play for "+viewtag);
+        // console.log("OLD_C ", oc);
+        // console.log("NEW_C ", nc);
 
         let anim = await gemini.animate(oc, nc, gs);
         await anim.play(viewtag);
+        return viewtag;
     }
 
     $: updateChart(static_col, var_col);
@@ -107,6 +114,6 @@
 
 <div class="chart_wrapper">
     <p>{'static: ' + static_col + ', var: ' + var_col}</p>
-    <!-- <div on:mouseenter id={'view_' + c_id} /> -->
-    <div id={'view_' + c_id} />
+    <div on:mouseenter id={'view_' + c_id} />
+    <!-- <div id={'view_' + c_id} /> -->
 </div>
